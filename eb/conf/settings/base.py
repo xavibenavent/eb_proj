80a2c630b3ec100041821b6be84500f2c0c43e7f
xbn <xbn@xbn.com>
2017-03-14 20:37:31 +0100

    added explanation about BASE_DIR  in base.py

--- eb/conf/settings/base.py
+++ eb/conf/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# It is the ~/projects/eb_proj/eb/ directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
